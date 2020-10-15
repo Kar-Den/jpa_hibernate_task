c2ae67110126ce0fc9bf5a479a4976471ee8d84c
Kar-Den <k.denis.vlad@gmail.com>
2020-10-15 12:19:13 +0300

    создал в даошке метод для сохранения работников списком

--- src/main/java/by/it/Runner.java
+++ src/main/java/by/it/Runner.java
@@ -6,8 +6,9 @@ import by.it.model.Employee;
 import by.it.model.EmployeePersonalInfo;
 import by.it.model.EmployeeStatus;
 import org.h2.tools.Server;
-import javax.persistence.EntityManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Runner {
 
@@ -22,68 +23,72 @@ public class Runner {
     }
 
     public static void main(String[] args) {
-        EntityManager em = JpaEntityManagerFactoryUtil.getEntityManager();
 
-        Employee employee = Employee.builder()
-                .name("Денис")
-                .surName("Карпенко")
-                .age(35)
-                .external(false)
-                .build();
+        List<Employee> employeesList = new ArrayList<>();
 
-        EmployeePersonalInfo personalInfo = EmployeePersonalInfo.builder()
-                .numberPassport("3210781PB12314")
-                .employeeStatus(EmployeeStatus.WORKING)
-                .address(Address.builder()
-                        .city("Гомель, п.Чёнки")
-                        .street("ул.Севруковская")
-                        .homNumber("10")
-                        .build())
-                .build();
-        employee.setPersonalInfo(personalInfo);
-
-        Employee employee1 = Employee.builder()
-                .name("Иван")
-                .surName("Семёнов")
-                .age(35)
-                .external(false)
-                .build();
-
-        EmployeePersonalInfo personalInfo1 = EmployeePersonalInfo.builder()
-                .numberPassport("3208057PИ987412")
-                .employeeStatus(EmployeeStatus.WORKING)
-                .address(Address.builder()
-                        .city("Гомель")
-                        .street("пр.Речицкий")
-                        .homNumber("99")
-                        .build())
-                .build();
-        employee1.setPersonalInfo(personalInfo1);
-
-        Employee employee2 = Employee.builder()
-                .name("Алексей")
-                .surName("Верезубов")
-                .age(30)
-                .external(false)
-                .build();
+        //Create Employee
+        {
+            Employee employee = Employee.builder()
+                    .name("Денис")
+                    .surName("Карпенко")
+                    .age(35)
+                    .external(false)
+                    .build();
+            EmployeePersonalInfo personalInfo = EmployeePersonalInfo.builder()
+                    .numberPassport("3210781PB12314")
+                    .employeeStatus(EmployeeStatus.WORKING)
+                    .address(Address.builder()
+                            .city("Гомель, п.Чёнки")
+                            .street("ул.Севруковская")
+                            .homNumber("1")
+                            .flatNumber("10")
+                            .build())
+                    .build();
+            employee.setPersonalInfo(personalInfo);
+            employeesList.add(employee);
 
 
-        EmployeePersonalInfo personalInfo2 = EmployeePersonalInfo.builder()
-                .numberPassport("8519674MB951753")
-                .employeeStatus(EmployeeStatus.WORKING)
-                .address(Address.builder()
-                        .city("Гомель")
-                        .street("Весёлая 1-А")
-                        .homNumber("55-ххх")
-                        .build())
-                .build();
+            employee = Employee.builder()
+                    .name("Иван")
+                    .surName("Семёнов")
+                    .age(35)
+                    .external(false)
+                    .build();
+            personalInfo = EmployeePersonalInfo.builder()
+                    .numberPassport("3208057PИ987412")
+                    .employeeStatus(EmployeeStatus.WORKING)
+                    .address(Address.builder()
+                            .city("Гомель")
+                            .street("пр.Речицкий")
+                            .homNumber("99")
+                            .flatNumber("33-k")
+                            .build())
+                    .build();
+            employee.setPersonalInfo(personalInfo);
+            employeesList.add(employee);
 
-        employee2.setPersonalInfo(personalInfo2);
 
+            employee = Employee.builder()
+                    .name("Алексей")
+                    .surName("Верезубов")
+                    .age(30)
+                    .external(false)
+                    .build();
+            personalInfo = EmployeePersonalInfo.builder()
+                    .numberPassport("8519674MB951753")
+                    .employeeStatus(EmployeeStatus.WORKING)
+                    .address(Address.builder()
+                            .city("Гомель")
+                            .street("Весёлая 1-А")
+                            .homNumber("55-ххх")
+                            .flatNumber("33-smile")
+                            .build())
+                    .build();
+            employee.setPersonalInfo(personalInfo);
+            employeesList.add(employee);
+        }
         EmployeeDao employeeDao = new EmployeeDao();
-        employeeDao.saveInDB(employee);
-        employeeDao.saveInDB(employee1);
-        employeeDao.saveInDB(employee2);
+        employeeDao.saveAllInDB(employeesList);
 
     }
 }
--- src/main/java/by/it/dao/EmployeeDao.java
+++ src/main/java/by/it/dao/EmployeeDao.java
@@ -2,15 +2,26 @@ package by.it.dao;
 
 import by.it.JpaEntityManagerFactoryUtil;
 import by.it.model.Employee;
-
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class EmployeeDao {
 
-    public void saveInDB(Employee employee){
+    public void saveInDB(Employee employee) {
         EntityManager em = JpaEntityManagerFactoryUtil.getEntityManager();
         em.getTransaction().begin();
         em.persist(employee);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void saveAllInDB(List<Employee> employees) {
+        EntityManager em = JpaEntityManagerFactoryUtil.getEntityManager();
+
+        em.getTransaction().begin();
+        for (Employee employee : employees) {
+            em.persist(employee);
+        }
         em.getTransaction().commit();
         em.close();
     }
